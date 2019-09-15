ab0c3667ce4dbbc3931bad5d579815149c755f5e
Nathan Perkins <nperkins487@gmail.com>
2019-09-15 14:25:24 -0700

    Set lastWaitStatus when exec fails, add more tests.

--- src/gosh/exec.go
+++ src/gosh/exec.go
@@ -7,7 +7,7 @@ import (
 	"syscall"
 )
 
-// Exec executes the given command
+// Exec executes the given command and updates the lastWaitStatus.
 func (g *Gosh) Exec(cmd string, argv []string) {
 	attr := &os.ProcAttr{
 		Files: []*os.File{
@@ -16,15 +16,17 @@ func (g *Gosh) Exec(cmd string, argv []string) {
 			g.errFile,
 		},
 	}
-	cmd, _ = exec.LookPath(cmd)
-	proc, err := os.StartProcess(cmd, argv, attr)
+	cmdPath, _ := exec.LookPath(cmd)
+	proc, err := os.StartProcess(cmdPath, argv, attr)
 	if err != nil {
-		fmt.Fprintf(g.errFile, "%s: %s\n", cmd, err)
+		fmt.Fprintf(g.errFile, "%s: no such file or directory\n", cmd)
+		g.lastWaitStatus = 0x100
 		return
 	}
 	stat, err := proc.Wait()
 	if err != nil {
 		fmt.Fprintf(g.errFile, "Could not wait for %s\n", cmd)
+		g.lastWaitStatus = 0x100
 		return
 	}
 	g.lastWaitStatus = stat.Sys().(syscall.WaitStatus)
--- src/gosh/exec_test.go
+++ src/gosh/exec_test.go
@@ -1,14 +1,57 @@
 package gosh
 
-import "testing"
+import (
+	"syscall"
+	"testing"
+)
 
-func TestExecEcho(t *testing.T) {
-	g, _ := newTestGosh()
-	g.Exec("echo", []string{"echo", "hi"})
-	got, _ := g.getOutput()
-	want := "hi\n"
-	if got != want {
-		t.Logf("For Exec(%q):", "echo hi")
-		t.Errorf("got %q, want %q", got, want)
+func TestExec(t *testing.T) {
+	tests := []struct {
+		name           string
+		cmd            []string
+		wantStdout     string
+		wantStderr     string
+		wantWaitStatus syscall.WaitStatus
+	}{
+		{
+			name:           "echo_hi",
+			cmd:            []string{"echo", "hi"},
+			wantStdout:     "hi\n",
+			wantWaitStatus: 0x00,
+		},
+		{
+			name:           "cat_example_txt",
+			cmd:            []string{"cat", "testdata/example.txt"},
+			wantStdout:     "just a few words\n",
+			wantWaitStatus: 0x00,
+		},
+		{
+			name:           "cat_invalid_file",
+			cmd:            []string{"cat", "badfile.txt"},
+			wantStderr:     "cat: badfile.txt: No such file or directory\n",
+			wantWaitStatus: 0x100,
+		},
+		{
+			name:           "bad_exec",
+			cmd:            []string{"badfile"},
+			wantStderr:     "badfile: no such file or directory\n",
+			wantWaitStatus: 0x100,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			g, _ := newTestGosh()
+			g.Exec(test.cmd[0], test.cmd)
+			stdout, stderr := g.getOutput()
+			if stdout != test.wantStdout {
+				t.Errorf("stdout = %q, want %q", stdout, test.wantStdout)
+			}
+			if stderr != test.wantStderr {
+				t.Errorf("stderr = %q, want %q", stderr, test.wantStderr)
+			}
+			if g.lastWaitStatus != test.wantWaitStatus {
+				t.Errorf("status = %#x, want %#x", g.lastWaitStatus, test.wantWaitStatus)
+			}
+		})
 	}
 }
